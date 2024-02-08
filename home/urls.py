a8be8631db1d180e335f158ec96a24ebe9bde923
hellotechmanish <mk772542@gmail.com>
2024-02-08 23:34:18 +0530

    second commit

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from django.urls import path
 from home import views
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('contact', views.contact, name='contact'),
+
     path('sign_in',views.sign_in,name='sign_in'),
     path('sign_up',views.sign_up,name='sign_up'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+def home(request):
+    # return HttpResponse("Hello world!")
+    return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
 # Create your views here.
 def sign_in(request):
     return render(request,"sign_in.html")
